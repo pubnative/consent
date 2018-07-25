94f482f7b415042fa20fc8f73e263b3952f3728f
Pavel Khusainov <pavel.khusainov@pubnative.net>
2018-07-25 09:58:26 +0200

    Godoc wording

--- doc.go
+++ doc.go
@@ -1,8 +1,7 @@
 /*
-Package consent contains IAB consent string encoding and decoding
-implementation.
+Package consent contains IAB consent string encode and decode implementations.
 
-To create a new consent use the consent.New function:
+Create a new consent with the consent.New function:
 
  cmpID := 1
  cmpVersion := 1
@@ -15,16 +14,19 @@ To create a new consent use the consent.New function:
  c1 := consent.New(cmpID, cmpVersion, consentScreen, lang,
  	vendorListVersion, purposesAllowed, allowedVendors)
 
-To decode an existing consent use consent.Parse:
+Decode an existing string with consent.Parse:
 
  c2, err := consent.Parse("BOQ7WlgOQ7WlgABACDENABwAAABJOACgACAAQABA")
 
-In both cases you can modify the consent struct as you want:
+In both cases you can read and modify a struct as you want:
 
+ if c2.Vendors[55] {
+	 // Vendor #55 is permitted
+ }
  c2.Vendors[999] = true
  c2.LastUpdated = time.Now()
 
-Then you can encode it as IAB base64 encoded string:
+At anytime export it as an IAB base64-encoded consent string:
 
  c2.String()
 
