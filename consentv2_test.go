00e0b78bc47a9cde57b9ceccef60780d4c3884a6
Ara Papikyan <ara.papikyan@verve.com>
2024-06-27 18:14:08 +0400

    working on tests for version 2.2

--- consentv2_test.go
+++ consentv2_test.go
@@ -123,3 +123,71 @@ func TestConsentV2ParsePublisherTC(t *testing.T) {
 
 	equal(t, c.String(), s)
 }
+
+func TestConsentV2_2Parse(t *testing.T) {
+	s := "COtwGEAPA9dwAKjACBENA6EIAEBAAECAAIAAAAAAAAAA.QAAA.IAAA"
+	c := ConsentV2{}
+
+	err := c.Parse(s)
+	noError(t, err)
+	equal(t, int(c.Version()), 2)
+	equal(t, c.Created.String(), "2020-01-26 04:00:00 +0400 +04")
+	equal(t, c.LastUpdated.String(), "2021-02-02 04:00:00 +0400 +04")
+	equal(t, c.CmpID, 675)
+	equal(t, c.CmpVersion, 2)
+	equal(t, int(c.ConsentScreen), 1)
+	equal(t, string(c.ConsentLanguage[:]), "EN")
+	equal(t, c.VendorListVersion, 58)
+	equal(t, int(c.TcfPolicyVersion), 4)
+	equal(t, c.IsServiceSpecific, false)
+	equal(t, c.UseNonStandardStacks, false)
+	equal(t, string(c.PublisherCC[:]), "AA")
+	equal(t, c.PurposeOneTreatment, true)
+	equal(t, c.SpecialFeatureOptIns, [12]bool{true, false})
+	equal(t, c.PurposesConsent, [24]bool{false, true, false, false, false, false, false, false, false, true})
+	equal(t, c.PurposesLITransparency, [24]bool{false, true, false, false, false, false, false, false, true})
+
+	// equal(t, c.String(), s)
+}
+
+func TestConsentV2_2ParseVendorLegitimateInterest(t *testing.T) {
+	s := "COrBaYAOrVMAAACABBENA6EIAD-AAAAAAACAAxQBQALgAlABeADUAMQBAwBQASgAvABqAGIAQIAA.QAAA.IAAA"
+	c := ConsentV2{}
+
+	err := c.Parse(s)
+	equal(t, err, nil)
+	equal(t, c.VendorLegitimateInterest.Set, map[int]bool{
+		37:  true,
+		47:  true,
+		53:  true,
+		98:  true,
+		129: true,
+	})
+
+	// equal(t, c.String(), s)
+}
+
+func TestConsentV2_2ParseDisclosedAndAllowedVendors(t *testing.T) {
+	s := "COrBaYAOrVMAAACABBENA6EAAAAAAAAAAACAAAAAAAAA.QAagAQAgAIAggAAA.IBAwBgALgAlABeADUAMQAgQA"
+	c := ConsentV2{}
+	err := c.Parse(s)
+	noError(t, err)
+	equal(t, c.AllowedVendors.Set, map[int]bool{
+		12: true,
+		23: true,
+		37: true,
+		47: true,
+		53: true,
+	})
+
+	equal(t, c.DisclosedVendors.Set, map[int]bool{
+		23:  true,
+		37:  true,
+		47:  true,
+		53:  true,
+		98:  true,
+		129: true,
+	})
+
+	// equal(t, c.String(), s)
+}
